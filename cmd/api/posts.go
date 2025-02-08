333c640af11d9b71adfd90642ce33f806ebd9908
ng1n3 <code.with.muyiwa@gmail.com>
2025-02-08 22:56:18 +0100

    feat: getbyid post and getPostHandler implemented

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/Ng1n3/social/internal/store"
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
@@ -35,6 +38,32 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
 		writeJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetByID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJsonError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSON(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
+		writeJsonError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 }
