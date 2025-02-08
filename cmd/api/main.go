01bedf8f1004da488258fd9330f9e94895861b3a
ng1n3 <code.with.muyiwa@gmail.com>
2025-02-08 12:44:00 +0100

    feat: repository pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,21 @@ import (
 	"log"
 
 	"github.com/Ng1n3/social/internal/env"
+	"github.com/Ng1n3/social/internal/store"
 )
 
 func main() {
 	cfg := config{
-		addr: env.GetString("ADDR", ":3050"),
+    addr: env.GetString("ADDR", ":3050"),
 	}
+  
+  store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+    store: store,
 	}
+
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
