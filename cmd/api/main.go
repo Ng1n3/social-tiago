8af9952fc44668de65350311be380bcc8fbc89ec
ng1n3 <code.with.muyiwa@gmail.com>
2025-02-08 15:07:10 +0100

    feat: configure db connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,10 +12,10 @@ func main() {
 	cfg := config{
     addr: env.GetString("ADDR", ":3050"),
     db: dbConfig{
-      addr: env.GetString("DB_ADDR", "postgress://user:superpassword@localhost/social?sslmode=disable"),
+      addr: env.GetString("DB_ADDR", "postgres://admin:superpassword@localhost:5434/social?sslmode=disable"),
       maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
       maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-      maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15min"),
+      maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15m"),
     },
 	}
 
@@ -23,12 +23,14 @@ func main() {
     cfg.db.addr,
     cfg.db.maxOpenConns,
     cfg.db.maxIdleConns,
-    cfg.db.maxIdleTime
+    cfg.db.maxIdleTime,
   )
 
   if err != nil {
     log.Panic(err)
   }
+  defer db.Close()
+  log.Println("database connection pool established")
   
   store := store.NewStorage(db)
 
