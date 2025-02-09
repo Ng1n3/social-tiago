ccffe96c077068e39cae225d4b8c1436359866d4
ng1n3 <code.with.muyiwa@gmail.com>
2025-02-09 20:56:15 +0100

    feat: update posts

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -94,6 +94,16 @@ func (s *PostStore) Delete(ctx context.Context, id int64) error {
 	return nil
 }
 
-func (s *PostStore) Update(ctx context.Context, id int64, post *Post) (*Post, error) {
-	query := ``
+func (s *PostStore) Update(ctx context.Context, post *Post) error {
+	query := `
+		UPDATE posts
+		SET title = $1, content = $2
+		WHERE id = $3
+	`
+
+	_, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
