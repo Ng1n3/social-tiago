ce0d074b7d8a53f296d81956c6faa10901f1e209
ng1n3 <code.with.muyiwa@gmail.com>
2025-05-20 16:35:38 +0100

    feat: comment out unused method

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -60,31 +60,31 @@ func getUserFromCtx(r *http.Request) *store.User {
 	return user
 }
 
-func (app *application) usersContextMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		idParams := chi.URLParam(r, "userID")
-		id, err := strconv.ParseInt(idParams, 10, 64)
-		if err != nil {
-			app.badRequestResponse(w, r, err)
-		}
+// func (app *application) usersContextMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		idParams := chi.URLParam(r, "userID")
+// 		id, err := strconv.ParseInt(idParams, 10, 64)
+// 		if err != nil {
+// 			app.badRequestResponse(w, r, err)
+// 		}
 
-		ctx := r.Context()
+// 		ctx := r.Context()
 
-		user, err := app.store.Users.GetByID(ctx, id)
-		if err != nil {
-			switch {
-			case errors.Is(err, store.ErrNotFound):
-				app.notFound(w, r, err)
-			default:
-				app.internalServerError(w, r, err)
-			}
-			return
-		}
+// 		user, err := app.store.Users.GetByID(ctx, id)
+// 		if err != nil {
+// 			switch {
+// 			case errors.Is(err, store.ErrNotFound):
+// 				app.notFound(w, r, err)
+// 			default:
+// 				app.internalServerError(w, r, err)
+// 			}
+// 			return
+// 		}
 
-		ctx = context.WithValue(ctx, UserCtx, user)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
+// 		ctx = context.WithValue(ctx, UserCtx, user)
+// 		next.ServeHTTP(w, r.WithContext(ctx))
+// 	})
+// }
 
 type FollowUser struct {
 	UserID int64 `json:"user_id"`
